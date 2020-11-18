0ad877622ae7a7000eea477f6709c10152370d72
Andreas Christian <achristian@de.ibm.com>
2020-11-18 11:37:41 +0100

    Create fetch_rows.go

---
+++ programs/fetch_rows.go
@@ -0,0 +1,41 @@
+// fetch_rows.go
+
+package main
+import (
+    _ "github.com/ibmdb/go_ibm_db"
+    "database/sql"
+    "fmt"
+)
+var err error
+var db *sql.DB
+var con = "HOSTNAME=localhost;PORT=50000;DATABASE=SAMPLE;UID=DB2INST1;PWD=db2inst1"
+
+func connect() error {
+        db, err = sql.Open("go_ibm_db", con)
+        if err != nil {
+                fmt.Println(err)
+                return err
+        }
+        return nil
+}
+func main() {
+        if connect() != nil { return } else { defer db.Close() }
+
+        rows,err := db.Query("select firstnme, lastname, job from employee where job='MANAGER'")
+        if err != nil {
+                return
+        }
+        // make sure that the "rows" handle is released when main returns
+        defer rows.Close()
+
+        // iterate over all rows in the query result
+        var a,b,c string
+        for rows.Next() {
+                err = rows.Scan(&a,&b,&c)
+                if err != nil{
+                        fmt.Println(err)
+                        return
+                }
+                fmt.Printf("%-10s %-10s %-10s\n",a,b,c)
+        }
+}
