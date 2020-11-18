21b3cb4aea25dc0ba60f3eb5f114f7db92aeeb51
Andreas Christian <achristian@de.ibm.com>
2020-11-18 11:45:15 +0100

    Create get_column_names.go

---
+++ programs/get_column_names.go
@@ -0,0 +1,41 @@
+// get_column_names.go
+
+package main
+import (
+    _ "github.com/ibmdb/go_ibm_db"
+    "database/sql"
+    "fmt"
+)
+
+var err error
+var db *sql.DB
+var con = "HOSTNAME=localhost;PORT=50000;DATABASE=SAMPLE;UID=DB2INST1;PWD=db2inst1"
+
+func connect() error {
+        db, err = sql.Open("go_ibm_db", con)
+        if err != nil {
+                fmt.Println(err)
+                return err
+        }
+        return nil
+}
+func main() {
+        if connect() != nil { return } else { defer db.Close() }
+
+        rows,err := db.Query("select * from employee fetch first 1 row only")
+        if err != nil {
+                fmt.Printf("db.Query(): error!")
+                return
+        }
+        // make sure that the "rows" handle is released when main returns
+        defer rows.Close()
+
+        cols, err := rows.Columns()
+        fmt.Println("Number of columns: ",len(cols))
+        // print the whole array at once
+        fmt.Printf("%v\n",cols)
+        // print each column name on a separate line
+        for _,name := range cols {
+                fmt.Printf("%s\n",name)
+        }
+}
